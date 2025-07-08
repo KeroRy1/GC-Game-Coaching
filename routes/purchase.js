c2751a7c82f90b76d21e66edc7322a5c966d99b0
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:55:54 +0300

    Update purchase.js

--- routes/purchase.js
+++ routes/purchase.js
@@ -1,1 +1,7 @@
+// server/routes/purchase.js
+const router = require('express').Router();
+const { initPurchase } = require('../controllers/purchaseController');
 
+router.post('/', initPurchase);
+
+module.exports = router;
