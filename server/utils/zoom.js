73a3477a58ac9070510313cbd4f5b33d283cd57c
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 13:27:59 +0300

    Delete server directory

--- server/utils/zoom.js
+++
@@ -1,6 +0,0 @@
-// server/utils/zoom.js
-console.log('[zoom stub] loaded from:', __filename);
-async function createZoomMeeting({ user, coach, game, slot }) {
-  return { join_url: 'https://zoom.us/j/0000000000' };
-}
-module.exports = { createZoomMeeting };
