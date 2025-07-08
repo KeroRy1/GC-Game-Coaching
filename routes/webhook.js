0182f888deb6b9ec64f964cb29579037c85a86d0
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:56:25 +0300

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,1 +1,7 @@
+// server/routes/webhook.js
+const router = require('express').Router();
+const shopierWebhook = require('../utils/shopierWebhook');
 
+router.post('/shopier', shopierWebhook.handle);
+
+module.exports = router;
