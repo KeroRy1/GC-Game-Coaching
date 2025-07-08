e040e6b98e8aecbe228e361d8905f70655c6de5e
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:56:13 +0300

    Update purchaseController.js

--- server/controllers/purchaseController.js
+++ server/controllers/purchaseController.js
@@ -1,1 +1,13 @@
+// server/controllers/purchaseController.js
+const Purchase = require('../models/Purchase');
+const shopierWebhook = require('../utils/shopierWebhook');
 
+async function initPurchase(req, res) {
+  const { userId, game, level, slot } = req.body;
+  const purchase = await Purchase.create({ user: userId, game, level, slot });
+  // Shopier ödeme linki üret
+  const paymentUrl = shopierWebhook.generateLink(purchase._id, game, level, slot);
+  res.json({ paymentUrl });
+}
+
+module.exports = { initPurchase };
