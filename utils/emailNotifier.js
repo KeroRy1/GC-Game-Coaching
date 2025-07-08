2ff694015cd1c2732bea246dd9e7862819bc2fe8
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:55:44 +0300

    Update emailNotifier.js

--- utils/emailNotifier.js
+++ utils/emailNotifier.js
@@ -1,1 +1,23 @@
+// server/utils/emailNotifier.js
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+function sendUserEmail(userEmail, meetingLink) {
+  const mailOptions = {
+    from: process.env.SMTP_USER,
+    to: userEmail,
+    subject: 'Koçluk Toplantı Linkiniz',
+    text: `Toplantınıza katılmak için tıklayın: ${meetingLink}`
+  };
+  return transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendUserEmail };
