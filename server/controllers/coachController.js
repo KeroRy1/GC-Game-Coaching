c96341ce989e72a140e1a6883dcdebfb8153d716
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:57:16 +0300

    Update coachController.js

--- server/controllers/coachController.js
+++ server/controllers/coachController.js
@@ -1,1 +1,14 @@
+// server/controllers/coachController.js
+const ZoomMeeting = require('../models/ZoomMeeting');
 
+async function getAssignedMeetings(req, res) {
+  const meetings = await ZoomMeeting.find({ coach: req.params.coachId });
+  res.json(meetings);
+}
+
+async function joinMeeting(req, res) {
+  const { meetingLink } = await ZoomMeeting.findById(req.body.meetingId);
+  res.json({ meetingLink });
+}
+
+module.exports = { getAssignedMeetings, joinMeeting };
