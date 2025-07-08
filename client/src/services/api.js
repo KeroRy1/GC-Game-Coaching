19bc98a87a25f395adc1ca85b5c2ac1255202229
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 13:27:40 +0300

    Delete client directory

--- client/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// client/src/services/api.js
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-export const initPurchase = data => API.post('/purchase', data);
-export const getMeetings = coachId => API.get(`/coach/${coachId}/meetings`);
-export const joinMeeting = (coachId, meetingId) => API.post(`/coach/${coachId}/join`, { meetingId });
