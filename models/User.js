3f4ac0054cfeb110f9c96224e315a6951d44ed0c
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:54:36 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
+// server/models/User.js
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  passwordHash: String,
+  role: { type: String, enum: ['user','coach','admin'], default: 'user' }
+});
+
+module.exports = mongoose.model('User', userSchema);
