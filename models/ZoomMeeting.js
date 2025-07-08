66a83a5e666c4c31ef19497a5b5b3a72b802360c
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:54:55 +0300

    Update ZoomMeeting.js

--- models/ZoomMeeting.js
+++ models/ZoomMeeting.js
@@ -1,1 +1,11 @@
+// server/models/ZoomMeeting.js
+const mongoose = require('mongoose');
 
+const zoomMeetingSchema = new mongoose.Schema({
+  meetingId: String,
+  meetingLink: String,
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: 'Coach' },
+  slot: String
+});
+
+module.exports = mongoose.model('ZoomMeeting', zoomMeetingSchema);
