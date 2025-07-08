5191e7081753e8fc31287f2b6bd2d16659f86ed1
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:55:32 +0300

    Update zoomClient.js

--- utils/zoomClient.js
+++ utils/zoomClient.js
@@ -1,1 +1,18 @@
+// server/utils/zoomClient.js
+const Zoom = require('zoomus')({
+  key: process.env.ZOOM_API_KEY,
+  secret: process.env.ZOOM_API_SECRET
+});
 
+async function createMeeting(slot) {
+  const res = await Zoom.meeting.create({
+    host_id: 'me',
+    topic: `Game Coaching ${slot}`,
+    type: 2,
+    start_time: new Date(),
+    duration: 60
+  });
+  return { meetingId: res.id, meetingLink: res.join_url };
+}
+
+module.exports = { createMeeting };
