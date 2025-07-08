64d02424049f9979f86edb0060fd5f2102d6d442
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 10:55:16 +0300

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,1 +1,10 @@
+// server/models/Feedback.js
+const mongoose = require('mongoose');
 
+const feedbackSchema = new mongoose.Schema({
+  purchase: { type: mongoose.Schema.Types.ObjectId, ref: 'Purchase' },
+  rating: Number,
+  comment: String
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
