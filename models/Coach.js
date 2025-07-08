b8bc640c4181286ab7f19a5d5bac6e4b7f65fbdd
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-08 13:27:46 +0300

    Delete models directory

--- models/Coach.js
+++
@@ -1,9 +0,0 @@
-// server/models/Coach.js
-const mongoose = require('mongoose');
-
-const coachSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  availableSlots: [String]  // ["08:00","09:00",...]
-});
-
-module.exports = mongoose.model('Coach', coachSchema);
